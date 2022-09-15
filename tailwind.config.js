ad0fbf24ca755cfab3be95f3033a98d618b43c0d
subhanfauzan <ariel1181987@gmail.com>
2022-09-15 12:18:10 +0700

    penambahan footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:  ['./src/**/*.{html,js}', 'index.html'],
+  content:  ['.**/*.{html,js}', 'index.html'],
   theme: {
     extend: {},
   },
